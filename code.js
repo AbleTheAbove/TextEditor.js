4b40aa1e6ab596e206f12e2ef04fe21095077bf2
Adam <39814353+ireallylikepies@users.noreply.github.com>
2021-12-27 12:18:52 +0000

    Update code.js

--- code.js
+++ code.js
@@ -1,33 +1,38 @@
+let activeTab = "1";
+let tabContentCache = {};
+
 document.getElementById("tabs").addEventListener('click', (event) => {
-	//console.log(event.target.outerHTML)
 	if ([...Array(10).keys()].map(i => i + 29).includes(event.offsetX) && [...Array(10).keys()].map(i => i + 11).includes(event.offsetY)) closeTab(event.target);
 })
 
 const closeTab = (tab) => {
-  console.log(tab.id)
   document.getElementById("tabs").innerHTML = document.getElementById("tabs").innerHTML.replace(tab.outerHTML, "");
-  document.getElementById("textareas").innerHTML = document.getElementById("textareas").innerHTML.replace("<textarea id=\""+ tab.id + "code\" class=\"code\" rows=\"60\" cols=\"201\" nowrap=\"nowrap\" wrap=\"off\" spellcheck=\"false\" onscroll=\"scrollNums(this)\"></textarea>", "")
 }
 
-const showTab = () => {
-  //hide current tab, show tab that was clicked on/just created
+const showTab = (tab) => {
+	let codebox = document.getElementById("code");
+	if (!tabContentCache["tab" + activeTab]) tabContentCache["tab" + activeTab] = {code: codebox.value}
+	else tabContentCache["tab" + activeTab] = {code: codebox.value}
+	codebox.value = tabContentCache[tab.id].code;
+	activeTab = tab.id.substring(3);
 }
 
-const newTab = (filename) => {
+const newTab = () => {
   let tabs = document.getElementById("tabs");
-  if (filename) {
-	  //stuff
+  document.getElementById("tabs").innerHTML += "<button id=\"tab" + (tabs.childElementCount + 1) + "\" class=\"tab\" onclick=\"showTab(this)\">•\xa0\xa0\xa0untitled</button>";
+  let codebox = document.getElementById("code");
+  if (!tabContentCache["tab" + activeTab]) tabContentCache["tab" + activeTab] = {
+	  code: codebox.value
   }
-  document.getElementById("tabs").innerHTML += "<button id=\"tab" + (tabs.childElementCount + 1) + "\" class=\"tab\" onclick=\"showTab()\">•\xa0\xa0\xa0untitled</button>";
-  let textareas = document.getElementById("textareas")
-  textareas.getElementsByClassName("active")[0].classList.remove("active");
-  textareas.innerHTML += " <textarea id=\"tab" + tabs.childElementCount + "code\" class=\"code active\" rows=\"60\" cols=\"201\" nowrap=\"nowrap\" wrap=\"off\" spellcheck=\"false\" onscroll=\"scrollNums(this)\"></textarea>";
+  else tabContentCache.code = codebox.value;
+  console.log(tabContentCache)
+  codebox.value = "";
+  activeTab = tabs.childElementCount;
 }
 
 const scrollNums = (codebox) => document.getElementById("nums").scrollTop = codebox.scrollTop;
 
 const initialise = () => {
-  console.log("here")
   let nums = [...Array(9999).keys()].map(i => i + 1).join("\n");
   document.getElementById("nums").value = nums;
   let tab = document.getElementById("tab1");
